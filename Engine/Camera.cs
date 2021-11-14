4df36d6466eb4ec3839e2932103885d18975958e
Guido Mintjes <guidomintjes@gmail.com>
2021-11-14 19:22:39 +0100

    cam reset fix

--- Engine/Camera.cs
+++ Engine/Camera.cs
@@ -27,7 +27,7 @@ namespace Engine {
 
             alive = true;
 
-            Reset();
+            CamReset();
         }
 
         /// <summary>
@@ -58,68 +58,9 @@ namespace Engine {
             return new Vector2(cameraPos.X, cameraPos.Y);
         }
 
-        public void Reset() {
+        public void CamReset() {
 
             cameraPos = new Point(0, 0);
         }
-
-        /*
-        Point worldSize;
-        Point windowSize;
-
-        SpriteGameObject camPosChanger;
-
-        public static Vector2 cameraChange;
-
-        public static bool alive = false; //TODO: Un-static?
-
-        Vector4 camOffset;
-
-        public static Matrix Transform { get; set; }
-
-        public Camera(Point _worldSize, Point newWindowSize, SpriteGameObject _camPosChanger, Vector4 _offset) {
-
-            camPosChanger = _camPosChanger;
-
-            Transform = new Matrix();
-
-            camOffset = _offset;
-
-            worldSize = _worldSize;
-            windowSize = newWindowSize;
-
-            alive = true;
-        }
-
-        public override void Update(GameTime gameTime) {
-
-            float newCamPosX = -camPosChanger.GlobalPosition.X - (camPosChanger.BoundingBox.Width / 2);
-            float newCamPosY = -camPosChanger.GlobalPosition.Y - (camPosChanger.BoundingBox.Height / 2);
-
-            newCamPosX = Math.Clamp(newCamPosX, -(worldSize.X - (windowSize.X / 2) + camOffset.W - camOffset.W * 1/1.15f), camOffset.X * 1.15f); //TODO: UNMAGIC
-            newCamPosY = Math.Clamp(newCamPosY, -700f, 0f);
-
-            Matrix position = Matrix.CreateTranslation(new Vector3(newCamPosX, newCamPosY, 0f));
-
-            Matrix offset = Matrix.CreateTranslation(new Vector3(
-                windowSize.X / 1.75f,
-                windowSize.Y / 1.75f,
-                0));
-
-            position.M44 = 1.15f; //TODO: UNMAGIC
-
-            Matrix newTransform = position * offset;
-
-            cameraChange = new Vector2(newTransform.M41 - Transform.M41, newTransform.M42 - Transform.M42);
-
-            Transform = newTransform;
-        }
-
-
-        public void Reset() {
-
-            Transform = new Matrix(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f);
-        }
-        */
     }
 }
